0047e2a02cf2b7ff0d782d71716488b7e584d078
JohnTardiff <46567849+JohnTardiff@users.noreply.github.com>
2023-02-03 16:29:32 -0600

    Build

--- ProjectTracker/ProjectTracker.cs
+++ ProjectTracker/ProjectTracker.cs
@@ -95,11 +95,14 @@ namespace ProjectTracker
             //Enter the current projects goals menu when the user enters a number input
             else if (int.TryParse(input, out projectIndex))
             {
-                Project project = projects[projectIndex];
-                string goalInput = "";
-                while (goalInput != BACK_STRING)
+                if(projectIndex < projects.Count())
                 {
-                    goalInput = ShowGoalsMenu(project);
+                    Project project = projects[projectIndex];
+                    string goalInput = "";
+                    while (goalInput != BACK_STRING)
+                    {
+                        goalInput = ShowGoalsMenu(project);
+                    }
                 }
             }
             //return input to check if the user entered EXIT or not
@@ -118,7 +121,7 @@ namespace ProjectTracker
                 output += counter + " - " + goal.ToString() + "\n";
                 counter++;
             }
-            output += "Enter the goal number, ADD to add a new goal, DELETE to delete the project, or BACK to go back to projects";
+            output += "Enter the goal number, ADD to add a new goal, DELETE to delete the project, EDIT to change the project name, or BACK to go back to projects";
             Console.WriteLine(output);
 
             //Navigate through the project's goal menu
@@ -133,12 +136,20 @@ namespace ProjectTracker
             //Show the goal update menu if the user enters an index of the project's goals list
             else if (int.TryParse(input, out goalIndex))
             {
-                Goal goal = project.goals[goalIndex];
-                string goalInput ="";
-                while (goalInput != BACK_STRING)
+                if (goalIndex < project.goals.Count())
                 {
-                    goalInput = ShowGoalUpdateMenu(project, goal);
+                    Goal goal = project.goals[goalIndex];
+                    string goalInput = "";
+                    while (goalInput != BACK_STRING)
+                    {
+                        goalInput = ShowGoalUpdateMenu(project, goal);
+                    }
                 }
+            }
+            //Change the project's name if the user enters EDIT
+            else if(input == EDIT_STRING)
+            {
+                EditProject(project);
             }
             //Delete the project if the user enters DELETE
             else if (input == DELETE_STRING)
@@ -184,22 +195,86 @@ namespace ProjectTracker
         //Method to prompt the user to enter a name for the new project and creates a new project
         private static void AddProject()
         {
-            //Clear the console and prompt the user for the new project's name
             Console.Clear();
-            Console.WriteLine("Enter the new project name");
+            string? input = null;
+            while (string.IsNullOrEmpty(input))
+            {
+                //Clear the console and prompt the user for the new project's name
+                Console.WriteLine("Enter the new project name, or BACK to cancel");
 
-            //Read the user's input and create a new project file based on the name entered
-            string? input = Console.ReadLine();
-            string projectFile = Path.Combine(projectDirectoryPath, input + ".txt");
-            FileStream create = File.Create(projectFile);
-            projects.Add(new Project(input, new FileInfo(projectFile)));
-            create.Close();
+                //Read the user's input and create a new project file based on the name entered
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input))
+                {
+                    if(projects.Find(x => x.GetName() == input) != null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Name already exists");
+                        input = null;
+                        continue;
+                    }
+                    if(input == BACK_STRING)
+                    {
+                        break;
+                    }
+                    string projectFile = Path.Combine(projectDirectoryPath, input + ".txt");
+                    FileStream create = File.Create(projectFile);
+                    projects.Add(new Project(input, new FileInfo(projectFile)));
+                    create.Close();
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name cannot be empty");
+                    continue;
+                }
+            }
         }
 
         //Method for creating projects based on the files read at startup
         private static void AddProject(string name, Goal[] goals, FileInfo projectFile)
         {
             projects.Add(new Project(name, goals, projectFile));
+        }
+
+        //Method for prompting the user to enter a new name for the project
+        private static void EditProject(Project project)
+        {
+            Console.Clear();
+            string? input = null;
+
+            while (string.IsNullOrEmpty(input))
+            {
+                //Clear the console and prompt the user for the goal's new name
+                Console.WriteLine("Enter the project's new name, or BACK to cancel");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    Project find = projects.Find(x => x.GetName() == input);
+                    if (find != null && find != project)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Name already exists");
+                        input = null;
+                        continue;
+                    }
+                    if(input == BACK_STRING)
+                    {
+                        input = project.GetName();
+                    }
+
+                    project.SetName(input);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name cannot be empty");
+                    continue;
+                }
+            }
         }
 
         //Method for prompting the user if they want to delete the project and deletes it if yes and doesn't if no
@@ -228,11 +303,38 @@ namespace ProjectTracker
         //Method for prompting the user to input the name of a new goal and creating it
         private static void AddGoal(Project project)
         {
-            //clea the console and prompt the user to enter the new goal's name
             Console.Clear();
-            Console.WriteLine("Enter the new goal name");
-            string input = Console.ReadLine();
-            project.AddGoal(input);
+            string? input = null;
+
+            while (string.IsNullOrEmpty(input))
+            {
+                //clear the console and prompt the user to enter the new goal's name
+                Console.WriteLine("Enter the new goal name, or BACK to cancel");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    if (project.goals.Find(x => x.GetName() == input) != null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Name already exists");
+                        input = null;
+                        continue;
+                    }
+                    if(input == BACK_STRING)
+                    {
+                        break;
+                    }
+                    project.AddGoal(input);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name cannot be empty");
+                    continue;
+                }
+            }
         }
 
         //Method for prompting the user for what the goal's new status should be
@@ -240,26 +342,37 @@ namespace ProjectTracker
         {
             //Clear the console and prompt the user to enter the goal's new status
             Console.Clear();
-            Console.WriteLine("Enter the new goal status: \n'0' for 'not yet started'\n'1' for 'in progress'\n'2' for 'completed'\nBACK to cancel");
-
-            //Read the user's input for a new status and set the goals new status
-            string? input = Console.ReadLine();
+            string? input = null;
             Goal.goalStatuses status = Goal.goalStatuses.Error;
-            switch (input)
+
+            while (string.IsNullOrEmpty(input))
             {
-                case "0":
-                    status = Goal.goalStatuses.Not_Yet_Started;
-                    break;
-                case "1":
-                    status = Goal.goalStatuses.In_Progress;
-                    break;
-                case "2":
-                    status = Goal.goalStatuses.Completed;
-                    break;
-                case BACK_STRING:
-                    status = goal.GetStatus();
-                    break;
+                Console.WriteLine("Enter the new goal status: \n'0' for 'not yet started'\n'1' for 'in progress'\n'2' for 'completed'\nBACK to cancel");
+
+                //Read the user's input for a new status and set the goals new status
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "0":
+                        status = Goal.goalStatuses.Not_Yet_Started;
+                        break;
+                    case "1":
+                        status = Goal.goalStatuses.In_Progress;
+                        break;
+                    case "2":
+                        status = Goal.goalStatuses.Completed;
+                        break;
+                    case BACK_STRING:
+                        status = goal.GetStatus();
+                        break;
+                    default:
+                        input = null;
+                        Console.Clear();
+                        Console.WriteLine("Invalid input");
+                        continue;
+                }
             }
+
             goal.SetStatus(status);
             project.SaveProject();
         }
@@ -267,14 +380,40 @@ namespace ProjectTracker
         //Method for prompting the user to enter a new name for the goal
         private static void EditGoal(Project project, Goal goal)
         {
-            //Clear the console and prompt the user for the goal's new name
             Console.Clear();
-            Console.WriteLine("Enter the goal's new name");
+            string? input = null;
 
-            //Read the user's input and set the goal's new name
-            string? input = Console.ReadLine();
-            goal.SetName(input);
-            project.SaveProject();
+            while (string.IsNullOrEmpty(input))
+            {
+                //Clear the console and prompt the user for the goal's new name
+                Console.WriteLine("Enter the goal's new name or BACK to cancel");
+                input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    Goal find = project.goals.Find(x => x.GetName() == input);
+                    if (find != null && find != goal)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Name already exists");
+                        input = null;
+                        continue;
+                    }
+                    if(input == BACK_STRING)
+                    {
+                        input = goal.GetName();
+                    }
+
+                    goal.SetName(input);
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Name cannot be empty");
+                    continue;
+                }
+            }
         }
 
 
